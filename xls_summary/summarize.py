ec44cb0af77eb4a02bc7ea0697cdf1aae948c716
Ng Oon-Ee <ngoonee@gmail.com>
2017-05-12 15:38:03 +0800

    Read the template to find locations

--- xls_summary/summarize.py
+++ xls_summary/summarize.py
@@ -1,5 +1,5 @@
 import os
-from openpyxl import Workbook
+from openpyxl import load_workbook
 from xlrd import open_workbook
 
 if __name__ == '__main__':
@@ -14,15 +14,26 @@ if __name__ == '__main__':
         print("Can't find a template, aborting")
         quit()
     cur_filelist.remove(template_name)
-    print(cur_filelist)
     # Create a summary file if it doesn't exist, otherwise load the current
     # one and append to it
     if summary_name in cur_filelist:
         # Load it up, then remove from cur_filelist
-        pass
+        cur_filelist.remove(summary_name)
     else:
         pass
     # Scan template file to find indicators
+    wb_t = load_workbook(os.path.join(my_dir, template_name))
+    locations = {}
+    for sheet_name in wb_t.get_sheet_names():
+        sheet = wb_t.get_sheet_by_name(sheet_name)
+        cells = sheet.get_cell_collection()
+        for c in cells:
+            if isinstance(c.value, str):
+                if c.value.startswith('::') and c.value.endswith('::'):
+                    row_identifier = c.value[2:-2]
+                    locations[row_identifier] = {'sheet_name': sheet_name,
+                                                 'col': c.column,
+                                                 'row': c.row}
     for f in cur_filelist:
         # Open file
         # Extract values at locations indicated in template file
